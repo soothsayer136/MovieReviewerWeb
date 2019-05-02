945553324275f4823f0d8641111558a73f96d06b
soothsayer136 <bikrum.katwal@gmail.com>
2019-05-02 08:26:58 +0545

    application server initialized

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log('Index is running');
-console.log('Index 2');
+// var application = require
+
+var express = require('express');
+
+var application = new express();
+
+
+
+
+
+application.listen(3001);
